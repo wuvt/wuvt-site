339b2b021ef12512ee09302a9006be7a3210df7d
mutantmonkey <mutantmonkey@mutantmonkey.in>
2013-01-05 11:46:28 +0100

    add ajax page loading (useful for live player)

--- wuvt/static/js/live.js
+++ wuvt/static/js/live.js
@@ -6,61 +6,73 @@ function wuvtLive(liveurl) {
             $('#current_track').text(msg['track']['artist'] + " - " +
                     msg['track']['title']);
             $('#current_dj').text(msg['track']['dj']);
+
+            if($('#trackable')) {
+                updateLast15(msg['track']);
+            }
         }
     };
 }
 
-function wuvtLiveLast15(liveurl) {
-    var source = new EventSource(liveurl);
-    source.onmessage = function(ev) {
-        msg = JSON.parse(ev.data);
-        if(msg['event'] == "track_change") {
-            $('#current_track').text(msg['track']['artist'] + " - " +
-                    msg['track']['title']);
-            $('#current_dj').text(msg['track']['dj']);
+function updateLast15(track) {
+    $('#tracktable tbody tr:last-child').remove();
 
-            var track = msg['track'];
-            $('#tracktable tbody tr:last-child').remove();
+    var tr = document.createElement('tr');
 
-            var tr = document.createElement('tr');
+    var td = document.createElement('td');
+    var playtime = track['datetime'].split(' ')[1];
+    playtime = playtime.split('.')[0];
+    $(td).text(playtime);
+    $(tr).append(td);
 
-            var td = document.createElement('td');
-            var playtime = track['datetime'].split(' ')[1];
-            playtime = playtime.split('.')[0];
-            $(td).text(playtime);
-            $(tr).append(td);
+    var td = document.createElement('td');
+    if(track['new'] == "true") {
+        $(td).text("NEw");
+    }
+    $(tr).append(td);
 
-            var td = document.createElement('td');
-            if(track['new'] == "true") {
-                $(td).text("NEw");
-            }
-            $(tr).append(td);
+    var td = document.createElement('td');
+    $(td).text(track['artist'] + " - " + track['title']);
+    $(tr).append(td);
 
-            var td = document.createElement('td');
-            $(td).text(track['artist'] + " - " + track['title']);
-            $(tr).append(td);
+    var td = document.createElement('td');
+    $(td).text(track['album']);
+    $(tr).append(td);
 
-            var td = document.createElement('td');
-            $(td).text(track['album']);
-            $(tr).append(td);
+    var td = document.createElement('td');
+    $(td).text(track['dj']);
+    $(tr).append(td);
 
-            var td = document.createElement('td');
-            $(td).text(track['dj']);
-            $(tr).append(td);
+    var td = document.createElement('td');
+    if(track['request'] == "true") {
+        $(td).text("REQ");
+    }
+    $(tr).append(td);
 
-            var td = document.createElement('td');
-            if(track['request'] == "true") {
-                $(td).text("REQ");
-            }
-            $(tr).append(td);
+    var td = document.createElement('td');
+    if(track['vinyl'] == "true") {
+        $(td).text("VIN");
+    }
+    $(tr).append(td);
 
-            var td = document.createElement('td');
-            if(track['vinyl'] == "true") {
-                $(td).text("VIN");
-            }
-            $(tr).append(td);
+    $('#tracktable tbody').prepend(tr);
+}
 
-            $('#tracktable tbody').prepend(tr);
-        }
-    };
+function makeAjaxLink(item) {
+    if($(item).attr('href').charAt(0) == '#') {
+        return;
+    }
+
+    $(item).click(function(ev) {
+        path = ev.target.href;
+        window.history.pushState({'path': path}, item.innerText, path);
+        loadPage(path);
+
+        return false;
+    });
+}
+
+function loadPage(path) {
+    $('#content').load(path + ' #content > *');
+    $.each($('#content a'), function(i, item){makeAjaxLink(item);});
 }
