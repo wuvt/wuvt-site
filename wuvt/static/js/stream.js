22a2eb3f7de62c03de87c583adbb65302dc96340
mutantmonkey <mutantmonkey@mutantmonkey.in>
2013-01-08 22:47:34 +0100

    add volume slider to live player

--- wuvt/static/js/stream.js
+++ wuvt/static/js/stream.js
@@ -41,8 +41,65 @@ function initPlayer() {
             streamPlaying = false;
             $('#stream_btn').removeClass('playing');
             $('#stream_btn').attr('title', "Play");
+
+            $('#volume_box').removeClass('visible');
         }
 
         return false;
+    });
+
+    initVolume();
+}
+
+function initVolume() {
+    var volbox = document.createElement('div');
+    $(volbox).attr('id', "volume_box");
+
+    var slider = document.createElement('div');
+    $(slider).attr('id', "volume_slider");
+    $(volbox).append(slider);
+
+    var mutebtn = document.createElement('button');
+    $(mutebtn).attr('id', "volume_mute_btn");
+    $(mutebtn).attr('title', "Mute");
+    $(mutebtn).text("Mute");
+    $(volbox).append(mutebtn);
+
+    $('#mainheader').append(volbox);
+
+    $('#volume_btn').click(function() {
+        var btn = $('#volume_btn');
+        var offsetTop = btn.offset().top + btn.outerHeight();
+        var offsetLeft = btn.offset().left + (btn.outerWidth() / 2) -
+            ($('#volume_box').outerWidth() / 2);
+
+        $('#volume_box').css('top', offsetTop + "px");
+        $('#volume_box').css('left', offsetLeft + "px");
+        $('#volume_box').toggleClass('visible');
+    });
+
+    $('#volume_slider').slider({
+        'orientation': "vertical",
+        'value': 1,
+        'step': 0.05,
+        'range': "min",
+        'max': 1,
+        'animate': false,
+        'slide': function(e, slider) {
+            $('#wuvt_stream').prop('muted', false);
+            $('#wuvt_stream').prop('volume', slider.value);
+        },
+    });
+
+    $('#volume_mute_btn').click(function() {
+        if(!$('#wuvt_stream').prop('muted')) {
+            $('#wuvt_stream').prop('muted', true);
+            $('#volume_slider').slider('option', 'value', 0);
+        }
+        else {
+            $('#wuvt_stream').prop('muted', false);
+            $('#volume_slider').slider('option', 'value',
+                $('#wuvt_stream').prop('volume'));
+        }
     });
 }
