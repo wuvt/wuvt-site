90dcd5257f68fab3ca857f07b279e91e15f3d5bd
mutantmonkey <mutantmonkey@mutantmonkey.in>
2015-03-12 21:29:17 -0700

    kill runserver.py
    WSGIServer is no good for our use, and I don't want people to be tempted
    to use runserver.py in production.

--- runserver.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python2
-
-from wuvt import app
-from gevent.wsgi import WSGIServer
-
-if __name__ == '__main__':
-    app.debug = True
-    server = WSGIServer(("", 8080), app)
-    server.serve_forever()
