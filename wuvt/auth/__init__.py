4975f8d15913254048f945d9551fa1ba4c20dbad
mutantmonkey <mutantmonkey@mutantmonkey.in>
2015-03-16 22:18:32 -0700

    move user loader into auth module

--- wuvt/auth/__init__.py
+++ wuvt/auth/__init__.py
@@ -1,5 +1,14 @@
 from flask import Blueprint
 
+from wuvt import login_manager
+from wuvt.models import User
+
 bp = Blueprint('auth', __name__)
+
+
+@login_manager.user_loader
+def load_user(userid):
+    return User.query.get(userid)
+
 
 from wuvt.auth import views
