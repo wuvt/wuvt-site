95cb5e4c7b8d2d4d2d0b8eb27739a27116295da4
mutantmonkey <mutantmonkey@mutantmonkey.in>
2015-03-22 22:50:58 -0700

    very basic partially working donation form

--- wuvt/donate/views.py
+++ wuvt/donate/views.py
@@ -1,1 +1,34 @@
+from flask import render_template, request, Response
+from wuvt import app
 from wuvt.donate import bp
+import stripe
+
+
+@bp.route('/')
+def donate():
+    return render_template('donate/premium_form.html')
+
+
+@bp.route('/process', methods=['POST'])
+def process():
+    stripe.api_key = app.config['STRIPE_SECRET_KEY']
+
+    amount = float(request.form['amount']) * 100
+    ship_premiums = request.form.get('ship_premiums', None) == "true"
+    if ship_premiums:
+        amount += int(app.config['DONATE_SHIPPING_COST']) * 100
+
+    try:
+        charge = stripe.Charge.create(
+            amount=amount,
+            currency="usd",
+            source=request.form['stripe_token'],
+            description=app.config['STRIPE_DESCRIPTION'])
+    except stripe.CardError, e:
+        int("Card declined!")
+        print(e)
+
+    # TODO: insert into database
+    # TODO: send thank you email
+
+    return Response("yes")
