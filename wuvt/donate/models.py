5f5f663fba40567af80ba32fa738cfeb4fff3a21
mutantmonkey <mutantmonkey@mutantmonkey.in>
2015-03-23 22:23:51 -0700

    donate: insert orders into database

---
+++ wuvt/donate/models.py
@@ -0,0 +1,71 @@
+import datetime
+from wuvt import db
+
+
+class Order(db.Model):
+    __tablename__ = 'orders'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Unicode(255))
+    email = db.Column(db.Unicode(255))
+    placed_date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+    # ip_address
+    dj = db.Column(db.UnicodeText)
+    thank_on_air = db.Column(db.Boolean, default=False)
+    first_time = db.Column(db.Boolean, default=True)
+
+    premiums = db.Column(db.Unicode(255))               #*mail, pickup, or none   
+    address1 = db.Column(db.Unicode(255))
+    address2 = db.Column(db.Unicode(255))
+    city = db.Column(db.Unicode(255))
+    state = db.Column(db.Unicode(255))
+    zipcode = db.Column(db.Integer)
+
+    amount = db.Column(db.Integer)                      # amount charged (in US cents)
+    recurring = db.Column(db.Boolean, unique=False, default=False) 
+    paid_date = db.Column(db.DateTime, default=None)
+    shipped_date = db.Column(db.DateTime, default=None)
+
+    tshirtsize = db.Column(db.Unicode(255))
+    tshirtcolor = db.Column(db.Unicode(255))
+
+    sweatshirtsize = db.Column(db.Unicode(255))
+    sweatshirtcolor = db.Column(db.Unicode(255))
+
+    method = db.Column(db.Unicode(255))
+    custid = db.Column(db.Unicode(255))                 # used for Stripe recurrence
+    comments = db.Column(db.UnicodeText)                # Internal use only. Staff can modify.
+
+    def __init__(self, name, email, dj, thank_on_air, first_time, amount,
+                 recurring):
+        self.name = name
+        self.email = email
+        self.dj = dj
+        self.thank_on_air = thank_on_air
+        self.first_time = first_time
+        self.amount = amount
+        self.recurring = recurring
+
+    def set_premiums(self, premiums, tshirtsize, tshirtcolor, sweatshirtsize,
+                     sweatshirtcolor):
+        self.premiums = premiums
+        self.tshirtsize = tshirtsize
+        self.tshirtcolor = tshirtcolor
+        self.sweatshirtsize = sweatshirtsize
+        self.sweatshirtcolor = sweatshirtcolor
+
+    def set_address(self, address1, address2, city, state, zipcode):
+        self.address1 = address1
+        self.address2 = address2
+        self.city = city
+        self.state = state
+        self.zipcode = zipcode
+
+    def set_paid(self, method, paid_date=None):
+        self.method = method
+
+        if paid_date is not None:
+            self.paid_date = paid_date
+        else:
+            self.paid_date = datetime.datetime.utcnow()
--- wuvt/donate/views.py
+++ wuvt/donate/views.py
@@ -1,11 +1,14 @@
+import stripe
 from flask import render_template, request, Response
 from wuvt import app
+from wuvt import db
 from wuvt.donate import bp
-import stripe
+from wuvt.donate.models import Order
 
 
 @bp.route('/')
 def donate():
+    # TODO: we need to recurring donations separately
     return render_template('donate/premium_form.html')
 
 
@@ -13,22 +16,47 @@ def donate():
 def process():
     stripe.api_key = app.config['STRIPE_SECRET_KEY']
 
-    amount = float(request.form['amount']) * 100
-    ship_premiums = request.form.get('ship_premiums', None) == "true"
-    if ship_premiums:
+    premiums = request.form.get('premiums', 'no')
+
+    amount = int(float(request.form['amount']) * 100)
+    if premiums == "ship" and amount >= app.config['DONATE_SHIPPING_MINIMUM']:
         amount += int(app.config['DONATE_SHIPPING_COST']) * 100
 
-    try:
-        charge = stripe.Charge.create(
-            amount=amount,
-            currency="usd",
-            source=request.form['stripe_token'],
-            description=app.config['STRIPE_DESCRIPTION'])
-    except stripe.CardError, e:
-        int("Card declined!")
-        print(e)
+    # FIXME: uncomment
+    #try:
+    #    charge = stripe.Charge.create(
+    #        amount=amount,
+    #        currency="usd",
+    #        source=request.form['stripe_token'],
+    #        description=app.config['STRIPE_DESCRIPTION'])
+    #except stripe.CardError, e:
+    #    int("Card declined!")
+    #    print(e)
 
-    # TODO: insert into database
+    order = Order(request.form['name'], request.form['email'],
+                  request.form.get('show', ''),
+                  request.form.get('onair', 'n') == 'y',
+                  request.form.get('firsttime', 'n') == 'y',
+                  amount,
+                  request.form.get('recurrence', 'once') == 'monthly')
+
+    if premiums != "no":
+        order.set_premiums(premiums,
+                           request.form.get('tshirtsize', None),
+                           request.form.get('tshirtcolor', None),
+                           request.form.get('sweatshirtsize', None),
+                           request.form.get('sweatshirtcolor', None))
+
+    if premiums == "ship":
+        order.set_address(request.form['address_1'], request.form['address_2'],
+                          request.form['city'], request.form['state'],
+                          request.form['zipcode'])
+
+    order.set_paid('stripe')
+
+    db.session.add(order)
+    db.session.commit()
+
     # TODO: send thank you email
 
     return Response("yes")
