9a8346846a78b3e867d9c78d80105b2ee35ea7bf
mutantmonkey <mutantmonkey@mutantmonkey.in>
2015-04-07 00:02:46 -0700

    move playlists by date callback code to init.js

--- wuvt/static/js/playlists_by_date_init.js
+++ wuvt/static/js/playlists_by_date_init.js
@@ -1,9 +1,2 @@
 var p = new PlaylistsByDate('#playlists_by_date');
 p.init();
-
-$(document).on('pageChange', function() {
-    if($('#playlists_by_date').length) {
-        var p = new PlaylistsByDate('#playlists_by_date');
-        p.init();
-    }
-});
--- wuvt/templates/init.js
+++ wuvt/templates/init.js
@@ -2,3 +2,10 @@ wuvtLive("{{ url_for('livestream') }}");
 initAjaxLinks();
 initPlayer();
 initLocalDates();
+
+$(document).on('pageChange', function() {
+    if($('#playlists_by_date').length) {
+        var p = new PlaylistsByDate('#playlists_by_date');
+        p.init();
+    }
+});
