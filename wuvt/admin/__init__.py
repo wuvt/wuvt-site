748e4d7e30e71f88095554db3d898c1702d17b50
Telnoratti <telnoratti@vtluug.org>
2015-10-25 02:35:17 -0400

    Add format_price and format_datetime to jinja context.

--- wuvt/admin/__init__.py
+++ wuvt/admin/__init__.py
@@ -3,3 +3,11 @@ from flask import Blueprint
 bp = Blueprint('admin', __name__)
 
 from wuvt.admin import views
+from wuvt import app
+from wuvt import format_datetime
+
+@app.context_processor
+def utility_processor():
+    def format_price(amount, currency='$'):
+        return u'{1}{0:.2f}'.format(amount/100, currency)
+    return dict(format_price=format_price, format_datetime=format_datetime)
