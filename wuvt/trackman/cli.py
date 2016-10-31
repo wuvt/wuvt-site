2a5e2315913782cedc33e4e4a56b307f14ed6390
mutantmonkey <mutantmonkey@mutantmonkey.in>
2016-10-30 23:00:06 -0700

    add some additional help text

--- wuvt/trackman/cli.py
+++ wuvt/trackman/cli.py
@@ -12,22 +12,26 @@ def trackman():
 
 @trackman.command()
 def deduplicate_all_tracks():
+    """Merge identical tracks."""
     lib.deduplicate_all_tracks()
 
 
 @trackman.command()
 def email_weekly_charts():
+    """If configured, email the weekly charts."""
     lib.email_weekly_charts()
 
 
 @trackman.command()
 def prune_empty_djsets():
+    """Prune empty DJSets from the database."""
     lib.prune_empty_djsets()
 
 
 @trackman.command()
 @click.option('--message', prompt=True)
 def send_message(message):
+    """Send a message to the current DJ."""
     r = redis_conn.publish('trackman_dj_live', json.dumps({
         'event': "message",
         'data': message,
