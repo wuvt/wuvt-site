c5d5af5d7bd932c5096aec2c617ba16813aceb90
mutantmonkey <mutantmonkey@mutantmonkey.in>
2016-12-02 01:08:11 -0800

    Use better range handling for charts
    * By month and year now mean by the calendar month and year instead of
      the past month/year.
    * URLs will now clearly indicate the range.
    * Constrain the ranges strictly to avoid being a potential DoS vector.

--- wuvt/trackman/charts.py
+++ wuvt/trackman/charts.py
@@ -7,38 +7,56 @@ CHART_PER_PAGE = 250
 CHART_TTL = 14400
 
 
-def get_range(period, request):
-    if period is not None:
+def get_range(period=None, year=None, month=None, week=None):
+    first_track = TrackLog.query.order_by(TrackLog.played).first()
+    if first_track is None:
+        raise ValueError("Cannot determine range without any logged tracks")
+
+    now = datetime.datetime.utcnow()
+
+    if period == 'weekly':
+        if year is not None or month is not None or week is not None:
+            raise ValueError(
+                "Weekly charts are available only for the current week.")
+
+        start = now - datetime.timedelta(weeks=1)
+        end = now
+    elif period == 'monthly':
+        if year is not None and month is not None:
+            start = datetime.datetime(year=year, month=month, day=1)
+            end = start + dateutil.relativedelta.relativedelta(months=1)
+        else:
+            start = datetime.datetime(year=now.year, month=now.month, day=1)
+            end = now
+    elif period == 'yearly':
+        if year is not None:
+            start = datetime.datetime(year=year, month=1, day=1)
+            end = start + dateutil.relativedelta.relativedelta(years=1)
+        else:
+            start = datetime.datetime(year=now.year, month=1, day=1)
+            end = now
+    elif period is None:
+        start = first_track.played
         end = datetime.datetime.utcnow()
-
-        if period == 'weekly':
-            start = end - datetime.timedelta(weeks=1)
-        elif period == 'monthly':
-            start = end - dateutil.relativedelta.relativedelta(months=1)
-        elif period == 'yearly':
-            start = end - dateutil.relativedelta.relativedelta(years=1)
-        else:
-            raise ValueError("Period not one of weekly, monthly, or yearly")
     else:
-        if 'start' in request.args:
-            start = datetime.datetime.strptime(request.args['start'],
-                                               "%Y-%m-%dT%H:%M:%S.%fZ")
-        else:
-            first_track = TrackLog.query.order_by(TrackLog.played).first()
-            if first_track is not None:
-                start = first_track.played
-            else:
-                start = datetime.datetime.utcnow()
-
-        if 'end' in request.args:
-            end = datetime.datetime.strptime(request.args['end'],
-                                             "%Y-%m-%dT%H:%M:%S.%fZ")
-        else:
-            end = datetime.datetime.utcnow()
+        raise ValueError(
+            "Period must be one of 'weekly', 'monthly', 'yearly', or None.")
 
     # reduce date resolution to 1 hour windows to make caching work
     start = start.replace(minute=0, second=0, microsecond=0)
     end = end.replace(minute=0, second=0, microsecond=0)
+
+    # enforce datetime boundaries
+    if start < first_track.played:
+        start = first_track.played
+    if end > now:
+        end = now
+
+    # sanity checking
+    if start > now:
+        raise ValueError("Start must be <= now")
+    if start > end:
+        raise ValueError("Start must be <= end")
 
     return start, end
 
