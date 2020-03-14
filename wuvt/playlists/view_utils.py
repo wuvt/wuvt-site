0d48e4bdfd9e74acc60e03fae89c76b6b364a55c
mutantmonkey <mutantmonkey@mutantmonkey.in>
2020-03-14 15:07:37 -0700

    Call .raise_for_status() for Trackman API

--- wuvt/playlists/view_utils.py
+++ wuvt/playlists/view_utils.py
@@ -11,8 +11,7 @@ def call_api(path, method, *args, **kwargs):
     path = path.format(*args, **kwargs)
     url = "{0}/api{1}".format(current_app.config['TRACKMAN_URL'], path)
     r = requests.request(method, url)
-    if r.status_code > 399:
-        abort(r.status_code)
+    r.raise_for_status()
     return r.json()
 
 
