b042eeefd71b370a1828f8860b95fb5669cc666f
echarlie <echarlie@vtluug.org>
2024-04-16 17:56:47 -0400

    create migration and regenerate requirements

---
+++ migrations/versions/28e9e049850c_add_pinned_articles.py
@@ -0,0 +1,25 @@
+"""add pinned articles
+
+Revision ID: 28e9e049850c
+Revises: 5981b26ae993
+Create Date: 2024-04-16 21:36:37.723311
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '28e9e049850c'
+down_revision = '5981b26ae993'
+
+from alembic import op
+import sqlalchemy as sa
+
+def upgrade():
+    op.add_column("article", sa.Column("pinned_article", sa.Boolean(), nullable=False, default=False))
+    op.add_column("article_revision", sa.Column("pinned_article", sa.Boolean(), nullable=False, default=False))
+    pass
+
+
+def downgrade():
+    op.drop_column("article", "pinned_article")
+    op.drop_column("article_revision", "pinned_article")
+    pass
